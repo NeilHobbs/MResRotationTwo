baba93afad35aae385960cd6cf4eee0d5603918d
NeilHobbs <neilphobbs@hotmail.com>
2020-04-27 12:35:12 +0100

    Added sigma (sd) into the function

--- IRtoSurvival.R
+++ IRtoSurvival.R
@@ -1,7 +1,7 @@
 #Function to translate IR levels into survival
-IR_Survival_Func = function(Kmax, z, n, z_50, nsim){ 
+IR_Survival_Func = function(Kmax, z, n, z_50, sigma,nsim){ 
   
-  f_z = rnorm(nsim, mean = z) #Generate random distribution around mean IR level, of nsim replicates. 
+  f_z = rnorm(nsim, mean = z, sd = sigma) #Generate random distribution around mean IR level, of nsim replicates. 
   #Should sigma be added for the standard deviation?
   
   for(i in 1:nsim){ ##for loop can be added if situation arises where z values are less than z
@@ -18,4 +18,5 @@ IR_Survival_Func = function(Kmax, z, n, z_50, nsim){
   #return(f_z)
 }
 
-IR_Survival_Func(1, 100, 1, 100, 1000)  #Currently only giving ~0.5 when n = 1
+IR_Survival_Func(1, 100, 1, 100, 5, 1000)  #Currently only giving ~0.5 when n = 1
+
