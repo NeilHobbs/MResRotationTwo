e1b017a27ea7efd47ef0e3a310154c3b85baf500
NeilHobbs <neilphobbs@hotmail.com>
2020-05-08 14:46:26 +0100

    Added additional labelling ability (CDC Bottles or WHO cylinders)

--- plot_survival_to_ir.R
+++ plot_survival_to_ir.R
@@ -8,9 +8,12 @@
 library(ggplot2)
 library(dplyr)
 
-
+##Where A can be 1 (CDC Bottle Bioassays)or 2 (WHO Cylinders)
 plot_survival_to_ir = function(Kmax, n, z_50, precision, sigma, nsim, min_value, max_value,
-                               minY, maxY, divisions){
+                               minY, maxY, divisions, A){
+  
+  assay.type = list("CDC Bottle Bioassay", "WHO Cylinders")
+  
   
   df=data.frame(Y.values=seq(minY, maxY, by = divisions))%>%
     rowwise%>%
@@ -23,12 +26,12 @@ plot_survival_to_ir = function(Kmax, n, z_50, precision, sigma, nsim, min_value,
   ggplot(df, aes(x=ir.values, y = Y.values)) +
     geom_point(colour = "red") +
     xlab("Insecticide Resistance Intensity (z)") +
-    ylab("Survival in CDC Bottle Bioassay")+
+    ylab(paste0("Survival in  ", assay.type[A]))+ ##May need changing to allow for description of what type of assay was used
     theme_classic()
 }
 
 plot_survival_to_ir(Kmax = 1, n=1, z_50 = 900, precision = 0.01, sigma = 25, nsim = 1000, min_value = 0,
-                    max_value = 25000, minY=0, maxY = 1, divisions = 0.01)
+                    max_value = 25000, minY=0, maxY = 1, divisions = 0.01, 2)
 
 
 
